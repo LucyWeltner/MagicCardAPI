9abea7992ca0fa3de925c9bac99e821a1fd07503
LucyWeltner <lucyiweltner@gmail.com>
2020-04-17 17:31:53 -0400

    Changed create method to render the deck just saved.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -6,6 +6,9 @@ class DecksController < ApplicationController
 	end
 
 	def create
-		Deck.create(params.permit(:name, :comments, :cards))
+		deck = Deck.create(params.permit(:name, :comments, :cards))
+		puts params[:cards]
+		params[:cards].each{|card| CardInDeck.create(card_id: card["id"], deck_id: deck.id)}
+		render json: {id: deck.id, name: deck.name, comments: deck.comments, cards: deck.cards}
 	end
 end
